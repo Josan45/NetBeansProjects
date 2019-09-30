1bb35f340cc0529daf4781ab9c956413a750fd47
Jose Antonio <joseantonioruiz04@gmail.com>
2019-09-30 10:42:18 +0200

    30/09/2019

--- Tema1/src/java/PrimerServlet/FormularioServlet.java
+++ Tema1/src/java/PrimerServlet/FormularioServlet.java
@@ -2,16 +2,13 @@ package PrimerServlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class PrimerServlet extends HttpServlet {
-
+public class FormularioServlet extends HttpServlet {
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -20,37 +17,45 @@ public class PrimerServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet PrimerServlet</title>");            
+            out.println("<title>Servlet FormularioServlet</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet PrimerServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet FormularioServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
     }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String nombre=request.getParameter("nombre");
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        String nom=request.getParameter("nombre");
+        String tef=request.getParameter("telefono");
+        String lugNaci=request.getParameter("lugar de nacimiento");
         
         response.setContentType("text/html");
         
         PrintWriter out=response.getWriter();
         
         out.println("<html>");
-        out.println("<head><title>Primer servlet</title></head>");
+        out.println("<head><title>Datos introducidos por Formulario</title></head>");
         out.println("<body>");
-        out.println("<h1>Hola, "+nombre+"</h1>");
-        out.println("<h1>"+getServletName()+" funcionando</h1>");
-        DateFormat dfEspañol=DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG,Locale.getDefault());
-        
-        out.println("<h3>Fehca: "+dfEspañol.format(new Date())+"</h3>");
-        out.println("Este es un servlet muy sencillo");
+        out.println("<h1>DATOS INTRODUCIDOS</h1>");
+        out.println("<table border=\"5\">");
+        out.println("<tr><td><b>NOMBRE:<b></td><td><i>"+nom+"</i></td></tr>");
+        out.println("<tr><td><b>TELEFONO:<b></td><td><i>"+tef+"</i></td></tr>");
+        out.println("<tr><td><b>LUGAR NACIMIENTO:<b></td><td><i>"+lugNaci+"</i></td></tr>");
+        out.println("</table><p>");
         out.println("</body>");
         out.println("</html>");
         
-        
-        //processRequest(request, response);
     }
 
     @Override
