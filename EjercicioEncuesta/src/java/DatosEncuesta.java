1c5ce233a43df22a8f98e6134e63e617155a3622
Jose Antonio <joseantonioruiz04@gmail.com>
2019-10-08 10:06:32 +0200

    08/10/2019

--- EjercicioEncuesta/src/java/DatosEncuesta.java
+++ EjercicioEncuesta/src/java/DatosEncuesta.java
@@ -19,41 +19,74 @@ public class DatosEncuesta extends HttpServlet {
         String texto=request.getParameter("texto");
         
         response.setContentType("text/html");
-        
-        PrintWriter out=response.getWriter();
-        
-        out.println("<html>");
-        out.println("<head><title>Datos introducidos por Formulario</title></head>");
-        out.println("<style> .nombre{color:red} .hom{background-color:cyan} .muj{background-color:pink}</style>");
-        out.println("<body>");
-        out.println("<h1>DATOS ENCUESTA</h1>");
-        out.println("<h2 class=nombre>Hola "+nom+"</h2>");
-        out.println("<table border=\"5\">");
-        out.println("<tr><td><b>Nombre:<b></td><td><i>"+nom+"</i></td></tr>");
-        out.println("<tr><td><b>Apellidos:<b></td><td><i>"+apel+"</i></td></tr>");
-        out.println("<tr><td><b>Telefono:<b></td><td><i>"+tel+"</i></td></tr>");
-        out.println("<tr><td><b>Lugar de Nacimiento:<b></td><td><i>"+ciu+"</i></td></tr>");
-        out.println("<tr><td><b>Sexo:<b></td><td><i>"+sex+"</i></td></tr>");
-        out.println("<tr><td><b>Pais de Origen:<b></td><td><i>"+pais+"</i></td></tr>");
-        if(texto.equals("")){
-            out.println("<tr><td>Area de Texto:<b></td><td><i>No tengo ningun comentario que mostrar</i></td></tr");
-        }else{  
-            out.println("<tr><td>Area de Texto:<b></td><td><i>"+texto+"</i></td></tr>");
-        }
-        out.println("</table>");
-        out.println("<p>"+nom+" tus actividades de ocio son: </p>");
-        
         if(sex.equals("Hombre")){
-            out.print("<body class=hom>");
-            out.println("<br><a href=tomcat.gif>Sorpresa</a>");
-        }else{
-            out.println("<body class=muj>");
-            out.println("<br><a href=taza.gif>Sorpresa</a>");
+            
+            PrintWriter out=response.getWriter();
+        
+            out.println("<html>");
+            out.println("<head><meta charset=\"UTF-8\"><title>Datos introducidos por Formulario</title></head>");
+            out.println("<style> .nombre{color:red} body{background-color:cyan}</style>");
+            out.println("<body>");
+            out.println("<h1>DATOS ENCUESTA</h1>");
+            out.println("<h2 class=nombre>Hola "+nom+"</h2>");
+            out.println("<table border=\"5\">");
+            out.println("<tr><td><b>Nombre:<b></td><td><i>"+nom+"</i></td></tr>");
+            out.println("<tr><td><b>Apellidos:<b></td><td><i>"+apel+"</i></td></tr>");
+            out.println("<tr><td><b>Telefono:<b></td><td><i>"+tel+"</i></td></tr>");
+            out.println("<tr><td><b>Lugar de Nacimiento:<b></td><td><i>"+ciu+"</i></td></tr>");
+            out.println("<tr><td><b>Sexo:<b></td><td><i>"+sex+"</i></td></tr>");
+            out.println("<tr><td><b>Pais de Origen:<b></td><td><i>"+pais+"</i></td></tr>");
+        
+            if(texto.equals("")){
+                out.println("<tr><td>Area de Texto:<b></td><td>No tengo ningun comentario que mostrar</td></tr");
+            }else{  
+                out.println("<tr><td>Area de Texto:<b></td><td>"+texto+"</td></tr>");
+            }
+            out.println("</table><br>");
+            out.println("<h4>"+nom+" tus actividades de ocio son: </h4>");
+            String[] casilla=request.getParameterValues("oc");
+            out.println("<table border=\"5\">");
+        
+            for(int x=0;x<casilla.length;x++){
+                out.println("<tr><td>"+casilla[x]+"</td></tr>");
+            }
+            out.println("</table>"); 
+            out.println("<br><a href=tomcat.gif>Sorpresa</a>");       
+            out.println("</html>");
+   
+            }else{
+            PrintWriter out=response.getWriter();
+        
+            out.println("<html>");
+            out.println("<head><meta charset=\"UTF-8\"><title>Datos introducidos por Formulario</title></head>");
+            out.println("<style> .nombre{color:red} body{background-color:pink}</style>");
+            out.println("<body>");
+            out.println("<h1>DATOS ENCUESTA</h1>");
+            out.println("<h2 class=nombre>Hola "+nom+"</h2>");
+            out.println("<table border=\"5\">");
+            out.println("<tr><td><b>Nombre:<b></td><td><i>"+nom+"</i></td></tr>");
+            out.println("<tr><td><b>Apellidos:<b></td><td><i>"+apel+"</i></td></tr>");
+            out.println("<tr><td><b>Telefono:<b></td><td><i>"+tel+"</i></td></tr>");
+            out.println("<tr><td><b>Lugar de Nacimiento:<b></td><td><i>"+ciu+"</i></td></tr>");
+            out.println("<tr><td><b>Sexo:<b></td><td><i>"+sex+"</i></td></tr>");
+            out.println("<tr><td><b>Pais de Origen:<b></td><td><i>"+pais+"</i></td></tr>");
+            if(texto.equals("")){
+                out.println("<tr><td>Area de Texto:<b></td><td>No tengo ningun comentario que mostrar</td></tr");
+            }else{  
+                out.println("<tr><td>Area de Texto:<b></td><td>"+texto+"</td></tr>");
+            }
+            out.println("</table>");
+            out.println("<h4>"+nom+" tus actividades de ocio son: </h4>");
+            String[] casilla=request.getParameterValues("oc");
+            out.println("<table border=\"5\">");
+
+            for(int x=0;x<casilla.length;x++){
+                out.println("<tr><td>"+casilla[x]+"</td></tr>");
+            }
+            out.println("</table>"); 
+            out.println("<br><a href=taza.gif>Sorpresa</a>");       
+            out.println("</html>");
         }
-        
-        out.println("</body>");
-        out.println("</html>");
-        
         
     }
 }
