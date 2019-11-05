26314aadb6b44f59a5e99e9d4c5ab1b06e7a9349
Jose Antonio <joseantonioruiz04@gmail.com>
2019-11-05 19:08:09 +0100

    .

---
+++ ConsultasActualizadas/src/consultasactualizadas/ConsultasActualizadas.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package consultasactualizadas;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ConsultasActualizadas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/ordenadores";
+        Connection con = null;
+
+        String borrado = "DELETE FROM ordenadores WHERE Unidades>60";
+        String mayor = "SELECT * FROM ordenadores";
+        String insertar = "UPDATE ordenadores SET Vendedores='yo'";
+        String cucu = "UPDATE ordenadores SET Vendedores='cucu' WHERE Unidades>55";
+
+        try {
+            Class.forName(driver);
+
+            con = DriverManager.getConnection(url, "root", "");
+
+            Statement st = con.createStatement();
+
+            ResultSet rs = st.executeQuery(mayor);
+            ResultSetMetaData meta = rs.getMetaData();
+            meta.getColumnCount();
+            String num;
+            String[] nomColum = new String[meta.getColumnCount() + 1];
+            //System.out.println(num);
+            int columna = meta.getColumnCount();
+
+            for (int x = 1; x <= meta.getColumnCount(); x++) {
+                num = meta.getColumnName(x);
+                nomColum[x] = num;//Guardo cada nombre de columnas
+                System.out.println(nomColum[x]);
+            }
+
+            int cont = 0;
+            int fila = 0;
+            System.out.println(columna);
+
+            //System.out.println(tabla[3][2]);
+            rs = st.executeQuery(mayor);
+            meta = rs.getMetaData();
+
+            while (rs.next()) {
+                fila++;
+            }
+            System.out.println(fila);
+
+            String tabla[][] = new String[fila][columna];
+
+            rs = st.executeQuery(mayor);
+
+            fila = 0;
+            while (rs.next()) {
+                for (int x = 1; x < columna + 1; x++) {
+                    tabla[(fila)][(x - 1)] = rs.getString(rs.getMetaData().getColumnName(x));
+                    //System.out.println(tabla[(fila)][(x-1)]);
+                }
+                fila++;
+            }
+            
+            st.executeUpdate(borrado);//Borro todos los que sus unidades sean mayores a 60
+
+            for (int x = 0; x < tabla.length; x++) {
+                String unidades = tabla[x][1];
+                int uni = Integer.parseInt(unidades);
+                if (uni > 60) {
+                        String añadir = "INSERT INTO ordenadores VALUE ('" + tabla[x][0] + "','" + tabla[x][1] + "','" + tabla[x][2] + "');";
+                        st.executeUpdate(añadir);
+                        //System.out.println();
+                }
+            }
+            st.executeUpdate(insertar);//Actualizo el nombre de vendedores a yo
+            st.executeUpdate(cucu);
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConsultasActualizadas.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ConsultasActualizadas.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
---
+++ CrearTablaConRegistro/src/creartablaconregistro/CrearTablaConRegistro.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package creartablaconregistro;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author alumno_2DAW
+ */
+public class CrearTablaConRegistro {
+
+    public static void main(String[] args) throws SQLException {
+
+        // TODO code application logic here
+       String driver="com.mysql.jdbc.Driver";
+        String url="jdbc:mysql://localhost:3306/mibasedatos";
+        Connection con=null;
+
+        Scanner dato = new Scanner(System.in);
+
+        System.out.println("¿Que opción elijes? ¿A o B?");
+        String opcion = dato.nextLine();
+
+        String op = opcion.toLowerCase();
+
+        String[] sentencia = new String[2];
+        String borrado="DROP TABLE mitabla";
+        String[] opc = new String[2];
+
+        int opcion1 = 0;
+
+        if (op.equals("A")) {
+            opcion1 = 0;
+        }
+
+        if (op.equals("B")) {
+            opcion1 = 1;
+        }
+
+        if (opcion1 == 0) {
+            sentencia[0] = "CREATE TABLE mitabla("
+                    + "nombre VARCHAR(45) NOT NULL,"
+                    + "apellidos VARCHAR(45) NOT NULL,"
+                    + "edad VARCHAR(45) NOT NULL,"
+                    + "telefono VARCHAR(45)"
+                    + ");";
+
+            opc[0] = "INSERT INTO mitabla VALUES ("
+                    + "'Jesus','Fernandez Ortega','28','941200639'),"
+                    + "('Pepe','Botella Ruiz','26','941212365'),"
+                    + "('Carlos','Ruiz Gil','26','941124541'"
+                    + ");";
+
+        } else {
+            sentencia[1] = "CREATE TABLE mitabla("
+                    + "nombre VARCHAR(45) NOT NULL,"
+                    + "apellidos VARCHAR(45) NOT NULL,"
+                    + "edad VARCHAR(45) NOT NULL,"
+                    + "lugarresidencia VARCHAR(45),"
+                    + "fechanacimiento DATE"
+                    + ");";
+
+            opc[1] = "INSERT INTO mitabla VALUES ("
+                    + "'Jesus','Fernandez Ortega','28','Logroño','1975-03-22'),"
+                    + "('Pepe','Botella Ruiz','26','Madrid','1975-10-12'),"
+                    + "('Carlos','Ruiz Gil','26','Barcelona','1976-11-25'"
+                    + ");";
+        }
+
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, "root", "");
+            Statement st = con.createStatement();
+            
+            st.executeUpdate(borrado);
+            st.executeUpdate(sentencia[opcion1]);
+            st.executeUpdate(opc[opcion1]);
+
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(CrearTablaConRegistro.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+}
